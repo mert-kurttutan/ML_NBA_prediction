1afd89b09e5aa0d26c11303b6bd7406c52ab102a
Mert Kurttutan <mertkurttutan@pop-os.localdomain>
2022-07-01 13:04:28 +0200

    Edited run_get_game_rotation script to make it compatible with entire pipeline

--- etl_training/ml-scripts/run_get_game_rotation.py
+++ etl_training/ml-scripts/run_get_game_rotation.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import json
+import argparse
 import requests
 import pandas as pd
 
@@ -9,8 +10,8 @@ from pathlib import Path
 
 
 # Local modules
-import nba_ml_package.utils as utils
-import nba_ml_package.etl as etl
+import nba_ml_module.utils as utils
+import nba_ml_module.etl as etl
 
 
 
@@ -21,7 +22,53 @@ GAME_ROTATION_DIR = "game_rotation_data"
 
 BUCKET_RAW = "mert-kurttutan-nba-ml-project-raw-data"
 BUCKET_CONFIG = "mertkurttutan-nba-ml-project-config"
+
 DATA_CONFIG_FILE = "data_config.json"
+
+
+# Make sure parent directories exists
+Path(f'{ROOT_DATA_DIR}/{GAME_ROTATION_DIR}/').mkdir(parents=True, exist_ok=True)
+
+
+def year_to_label(year: list):
+
+  return f"{year}-{str(year+1)[-2:]}", f"{year}-{str(year+1)}"
+
+def extract_data_from_s3(year_arr=None):
+  global LABEL_ARR
+  LABEL_ARR = None
+
+  LABEL_ARR = [year_to_label(year) for year in year_arr]
+  
+  # Copy config file from s3 bucket
+  os.system(f"aws s3 cp s3://{BUCKET_CONFIG}/{DATA_CONFIG_FILE} {ROOT_DATA_DIR}/{DATA_CONFIG_FILE} >> ~/entrypoint_log.log ")
+
+
+
+
+def define_config_vars():
+  print("Defining config vars...")
+  global CONFIG_DICT
+  global YEAR_ARR
+
+
+  with open(f"{ROOT_DATA_DIR}/{DATA_CONFIG_FILE}", "r") as read_content:
+    CONFIG_DICT = json.load(read_content)
+
+
+  # Create season from data_config
+  
+  if LABEL_ARR is None: 
+    YEAR_ARR = sorted(set([elem[:-4] for elem in list(CONFIG_DICT["season_dates"].keys())]))
+  
+  else:
+    LABEL_ARR_0 = [label[0] for label in LABEL_ARR]
+    YEAR_ARR = sorted(set([elem[:-4] for elem in list(CONFIG_DICT["season_dates"].keys()) if elem[:-4] in LABEL_ARR_0]))
+
+
+  print("Defining important dataframes...")
+
+
 
 
 def run_extract_game_rotation(gameId: str, proxy_config: dict, season_label: str):
@@ -29,7 +76,7 @@ def run_extract_game_rotation(gameId: str, proxy_config: dict, season_label: str
     Stores the data into player_stat_data directory"""
 
   df_file_name = f'{ROOT_DATA_DIR}/{GAME_ROTATION_DIR}/{season_label}/game_rotation_gameId{gameId}.csv'
-      
+  proxy_config = {}
   # Query data only if it does not exist
   if not os.path.exists(df_file_name):
     df_arr = etl.jsonToDataFrame(etl.getRawGameRotation(gameId=gameId, proxy_config=proxy_config))
@@ -85,31 +132,33 @@ def run_extract_game_rotation_array(game_id_arr: list, config_dict: dict, season
 
 if __name__ == "__main__":
     
-  os.system(f"aws s3 cp s3://{BUCKET_CONFIG}/{DATA_CONFIG_FILE} {ROOT_DATA_DIR}/{DATA_CONFIG_FILE}")
-    
-  with open(f"{ROOT_DATA_DIR}/{DATA_CONFIG_FILE}", "r") as read_content:
-    config_dict = json.load(read_content)
+  parser = argparse.ArgumentParser(description='Arguments for mlflow python script')
+  parser.add_argument("--year-arr", nargs="+", default=["2014"], help="List of years to process data of ")
+  value = parser.parse_args()
+
+  year_arr = [int(year) for year in value.year_arr]
+
+
+  extract_data_from_s3(year_arr=year_arr)
+
+  define_config_vars()
   
     
     
-  config_dict["uploaded_dir"] = []
+  for year in year_arr:
+    for season_label in sorted([elem for elem in list(CONFIG_DICT["season_game_ids"].keys())]):
+      season = utils.get_season_str(year)
+      if season in season_label:
+        print(season_label)
     
-  for season_label in sorted([elem for elem in list(config_dict["season_game_ids"].keys())]):
+        game_id_arr = CONFIG_DICT["season_game_ids"][season_label]
+        print(f"Query for season: {season_label}")
+        run_extract_game_rotation_array(game_id_arr=game_id_arr, config_dict=CONFIG_DICT, season_label=season_label)
     
-    # Check 1st-level subdirectories (prefixes) 
-    # If the current season is already uploaded to S3, skip this season
-    if f"{GAME_ROTATION_DIR}/{season_label}" in utils.get_subdirs_s3(BUCKET_RAW):
-      print(f"Directory {GAME_ROTATION_DIR}/{season_label} is already uploaded to S3")
-      continue
-    
-    game_id_arr = config_dict["season_game_ids"][season_label]
-    print(f"Query for season: {season_label}")
-    run_extract_game_rotation_array(game_id_arr=game_id_arr, config_dict=config_dict, season_label=season_label)
-    
-    # Upload directory to s3 bucket
-    # Exclude jupyter checkpoint files
-    os.system(f"aws s3 cp {ROOT_DATA_DIR}/{GAME_ROTATION_DIR} s3://{BUCKET_RAW}/{GAME_ROTATION_DIR}/ --recursive --exclude \".ipynb_checkpoints/*\" " )
-    
-    # After uploading this directory, delete it
-    # Since its presence is not needed anymore
-    os.system(f"rm -rf {ROOT_DATA_DIR}/{GAME_ROTATION_DIR}/*")
+  # Upload directory to s3 bucket
+  # Exclude jupyter checkpoint files
+  os.system(f"aws s3 cp {ROOT_DATA_DIR}/{GAME_ROTATION_DIR} s3://{BUCKET_RAW}/{GAME_ROTATION_DIR}/ --recursive --exclude \".ipynb_checkpoints/*\" " )
+  
+  # After uploading this directory, delete it
+  # Since its presence is not needed anymore
+  os.system(f"rm -rf {ROOT_DATA_DIR}/{GAME_ROTATION_DIR}/*")
